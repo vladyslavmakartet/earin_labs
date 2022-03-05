da41694d9cd56cf482df4a1cb647c1c275aea70b
Provagus <44500710+Provagus@users.noreply.github.com>
2022-03-05 14:09:58 +0100

    G function

--- lab1/equation.py
+++ lab1/equation.py
@@ -1,14 +1,22 @@
 from typing import Type
-
+import numpy
 
 class Function_Generic():
-    type = None
+    return_type = None
+    x_type = None
 
     def get_return_type(self) -> Type:
-        return self.type
+        return self.return_type
+    
+    def get_value(self) -> None:
+        pass
+
+    def get_x_type(self) -> Type:
+        return self.x_type
 
 class Function_F(Function_Generic):
-    type = int
+    return_type = int
+    x_type = int
 
     def __init__(self, a: int, b: int, c: int, d: int) -> None:
         self.a = a
@@ -19,3 +27,18 @@ class Function_F(Function_Generic):
     def get_value(self, x: int) -> int:
         result = self.a * x**3 + self.b * x**2 + self.c * x + self.d
         return result
+
+class Function_G(Function_Generic):
+    return_type = int
+    x_type = numpy.matrix
+
+    def __init__(self,  a: numpy.matrix, b: numpy.matrix, c: int) -> None:
+        self.c = c
+        self.b = b
+        self.a = a
+
+    def get_value(self, x: numpy.matrix) -> int:
+        b_part = self.b.transpose() * x
+        a_part = x.transpose() * self.a * x
+        result = self.c + b_part + a_part
+        return numpy.asscalar(result)
--- lab1/main.py
+++ lab1/main.py
@@ -1,9 +1,17 @@
+import numpy
 import equation
 
 def main() -> None:
     example_function = equation.Function_F(1, 2, 3, 4)
-    print("Type:", example_function.get_return_type())
-    print("Value for 2:", example_function.get_value(2))
+    print("F Type:", example_function.get_x_type())
+    print("F Value for 2:", example_function.get_value(2))
+    a = numpy.matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    b = numpy.matrix([[1], [2], [3]])
+    c = 2
+    x = numpy.matrix([[1], [2], [3]])
+    example_g = equation.Function_G(a, b, c)
+    print("G Type:", example_g.get_x_type())
+    print("G Value:", example_g.get_value(x))
 
 if __name__ == "__main__":
     main()
