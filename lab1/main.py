4607b65b7ab0cf36a05877f27762408a2a6e4165
Provagus <44500710+Provagus@users.noreply.github.com>
2022-03-11 11:20:05 +0100

    UI as main

--- lab1/main.py
+++ lab1/main.py
@@ -1,27 +1,7 @@
-import numpy
-import equation
-from methods import NewtonMethod, GradientDescent, BREAK_ITERATIONS
+from ui import ui
 
 def main() -> None:
-    example_function = equation.Function_F(0.25, 2, 3, 1)
-    print("F Type:", example_function.get_x_type())
-    print("F Value for 2:", example_function.get_value(2))
-    print("F Derivative for 2:", example_function.get_gradient_value(2))
-    a = numpy.matrix([[9, 1, 3], [1, 5, 4], [5, 8, 9]])
-    b = numpy.matrix([[1], [2], [4]])
-    c = 1
-    x = numpy.matrix([[1], [2], [1]])
-    example_g = equation.Function_G(a, b, c)
-    print("G Type:", example_g.get_x_type())
-    print("G Value:", example_g.get_value(x))
-    print("G Derivative:", example_g.get_gradient_value(x))
-    
-    print('============================================================')
-    print(GradientDescent.calculate_minimum(example_function, 1, BREAK_ITERATIONS, 200))
-    print(NewtonMethod.calculate_minimum(example_function, 1, BREAK_ITERATIONS, 200))
-    print('============================================================')
-    print(GradientDescent.calculate_minimum(example_g, numpy.matrix([[2], [2], [3]]), BREAK_ITERATIONS, 200))
-    print(NewtonMethod.calculate_minimum(example_g, numpy.matrix([[2], [2], [3]]), BREAK_ITERATIONS, 200))
-    print('============================================================')
+    ui()
+
 if __name__ == "__main__":
     main()
