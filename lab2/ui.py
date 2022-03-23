99afa65eab927efc754571f33a69e30d24e13d4c
Vladyslav Makartet <vladyslav.makartet@gmail.com>
2022-03-23 22:17:45 +0100

    Add ui

---
+++ lab2/ui.py
@@ -0,0 +1,101 @@
+from ui_common import getNumericScalar, format_input, is_symmetric, is_positive_definite, print_parameters
+import numpy as np
+import sys
+import equation
+import RouletteWheelSelection
+
+header_text = '''
+===============================================================================
+#   Genetic algorithm for analysis of multidimensional quadratic function     #
+===============================================================================
+#   Authors: Grzegorczyk Patryk, Vladyslav Makartet                           #
+===============================================================================
+'''
+separation_line = '===============================================================================\n'
+yes = ['y', 'yes', 'Y', 'Yes']
+# function_type = '''
+# F(x) = c+(b^T)*x+(x^T)*A*x (c - scalar number, b - vector of n numbers, A - 𝑛 𝑥 𝑛 matrix, x - vector)
+# '''
+ 
+def ui():
+    params = {}  # dict of all inputs
+    print(chr(27) + "[2J")  # clear output
+    print(header_text)
+    while True:
+        try:
+            params["c"] = getNumericScalar("c")
+            in_text = input('Please input vector B (i.e.: 1,2,3): ')
+            vector_b = format_input(in_text, ',')
+            vector_b = [[i] for i in vector_b]
+            vector_len = len(vector_b)
+            vector_b = np.asmatrix(vector_b).astype(
+                float)
+            vector_temp = []
+
+            print('Please input symmetric matrix with dimension {n}x{n}'.format(
+                n=vector_len) + ': ')
+            for x in range(0, vector_len):
+                in_text = input(
+                    '= Please input {n}th row of matrix A (i.e.: 1,2,3): '.format(n=x))
+                temp_text = format_input(in_text, ',')
+                assert len(temp_text) == vector_len, (
+                    f"Please input symmetric matrix with dimension {vector_len}x{vector_len}")
+                vector_temp.append(temp_text)
+
+            matrix_a = np.asmatrix(vector_temp).astype(
+                float)
+            if not (is_symmetric(matrix_a) and is_positive_definite(matrix_a)):
+                raise ValueError
+            
+            params["b"] = vector_b
+            params["a"] = matrix_a
+            print("\n" + separation_line)
+
+            params["d"] = getNumericScalar("d", "int")
+            params["dimensionality"] = getNumericScalar("dimensionality", "int")
+            params["population_size"] = getNumericScalar("population size", "int")
+            params["crossover_proba"] = getNumericScalar("crossover probability")
+            params["mutation_proba"] = getNumericScalar("mutation probability")
+            params["iter_num"] = getNumericScalar("number of iteration", "int")
+            
+        
+        
+        
+        except AssertionError as e:
+            print(f"ERROR: {e}\n")
+            continue
+        except ValueError as e:
+            print(f"ERROR: {e}\n")
+            continue
+        except np.linalg.LinAlgError:
+            print(
+                'Defined matrix is not a positive-definite! Please input a valid one and try again!\n')
+            continue
+        break
+
+
+    print(separation_line)
+    print_parameters(params)
+    run_program(params)
+
+    ans = input('=   Would you like to input new variables (y | n): ')
+    if ans in yes:
+        ui()
+    else:
+        sys.exit()
+
+def run_program(params: dict):
+    print(separation_line)
+    
+    function_to_process = equation.Function_G(params["a"],params["b"],params["c"])
+    RouletteWheelSelection.run_algorithm(params["dimensionality"],
+                                         params["population_size"],
+                                         params["d"],
+                                         params["mutation_proba"],
+                                         params["crossover_proba"],
+                                         params["iter_num"],
+                                         function_to_process)    
+    
+        
+if __name__ == "__main__":
+    ui()
---
+++ lab2/ui_common.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+
+def getNumericScalar(coef: str, type_="numeric", onlyPositive=False) -> float:
+    while True:
+        try:
+            if type_ == "int":
+                number = int(input(f'Please enter {coef} ({type_}): '))
+                if onlyPositive and number < 0:
+                    number = 0
+                    print("Value must be positive. Your input was set to zero.\n")
+            else:
+                number = float(input(f'Please enter {coef} ({type_}): '))
+
+        except ValueError:
+            print(f'ERROR: Input must be a {type_} value! Please try again!\n')
+            continue
+        break
+    return number
+
+
+def format_input(text, delimiter) -> list:
+    text = text.replace(' ', '')
+    text = text.split(delimiter)
+    text = [float(i) for i in text]
+    return text
+
+
+def is_symmetric(A):
+    return A.transpose().all() == A.all()
+
+
+def is_positive_definite(A):
+    np.linalg.cholesky(A)
+    return True
+
+
+def print_parameters(params: dict):
+    text_with_variables = f"""    scalar c: {params['c']}
+        vector b: {params['b']}
+        matrix A:\n {params['a']}
+        int d: {params["d"]}
+        problem dimensionality: {params["dimensionality"]}
+        population size: {params["population_size"]}
+        crossover probability: {params["crossover_proba"]}
+        mutation probability: {params["mutation_proba"]}
+        number of iterations: {params["iter_num"]}"""
+
+    print(f"""You entered the following:
+= Defined variables:
+    {text_with_variables}""")
+    input("Press Enter to continue...")
