9db8d51444cfca4d5ab65f270dbc5b3a3840e6f7
Provagus <44500710+Provagus@users.noreply.github.com>
2022-04-08 11:41:21 +0200

    Note about tutorial

--- lab3/main.py
+++ lab3/main.py
@@ -1,4 +1,8 @@
 from ui import main
 
+# During development we followed this tutorial:
+# https://www.youtube.com/watch?v=Bk9hlNZc6sE
+# UI and structure of the game was written following it
+
 if __name__ == "__main__":
     main()
